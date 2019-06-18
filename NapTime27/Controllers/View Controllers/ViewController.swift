c148f9714357cc005dfc6f820a6730991daed5ce
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-06-18 10:48:03 -0600

    Basic StoryBoard Structure and IBs

---
+++ NapTime27/Controllers/View Controllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NapTime27
+//
+//  Created by Timothy Rosenvall on 6/18/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var napButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func napButtonTapped(_ sender: Any) {
+    }
+}
